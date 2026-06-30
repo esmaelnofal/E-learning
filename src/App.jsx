b9e13832d5c524e4ff0837f3a44350d3be4aac74
sharkawy <ahmedelsharkawy7>
2026-06-30 21:59:21 +0300

    routes is added and made exam page

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,9 @@ import { useState } from "react";
 import Sidebar from "./components/Sidebar";
 import AITutorPage from "./components/AITutorPage";
 import ExamPage from "./components/ExamPage";
+import { RouterProvider } from "react-router";
+import { router } from "./router";
 
 export default function App() {
-  const [view, setView] = useState("tutor"); // 'tutor' | 'exam'
-  const [isDarkMode, setIsDarkMode] = useState(false);
-
-  return (
-    <div
-      className={`min-h-screen w-full ${isDarkMode ? "dark" : ""} font-sans`}
-    >
-      {view === "tutor" ? (
-        <div className="flex h-screen overflow-hidden pl-64">
-          <Sidebar />
-          <AITutorPage
-            toggleDarkMode={() => setIsDarkMode(!isDarkMode)}
-            isDarkMode={isDarkMode}
-            onStartExam={() => setView("exam")}
-          />
-        </div>
-      ) : (
-        <ExamPage
-          toggleDarkMode={() => setIsDarkMode(!isDarkMode)}
-          isDarkMode={isDarkMode}
-          onBack={() => setView("tutor")}
-        />
-      )}
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
---
+++ src/pages/Home2.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import Sidebar from "../components/Sidebar";
+import AITutorPage from "../components/AITutorPage";
+import ExamPage from "../components/ExamPage";
+
+export default function Home2() {
+  const [view, setView] = useState("tutor"); // 'tutor' | 'exam'
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  return (
+    <div
+      className={`min-h-screen w-full ${isDarkMode ? "dark" : ""} font-sans`}
+    >
+      {view === "tutor" ? (
+        <div className="flex h-screen overflow-hidden pl-64">
+          <Sidebar />
+          <AITutorPage
+            toggleDarkMode={() => setIsDarkMode(!isDarkMode)}
+            isDarkMode={isDarkMode}
+            onStartExam={() => setView("exam")}
+          />
+        </div>
+      ) : (
+        <ExamPage
+          toggleDarkMode={() => setIsDarkMode(!isDarkMode)}
+          isDarkMode={isDarkMode}
+          onBack={() => setView("tutor")}
+        />
+      )}
+    </div>
+  );
+}
